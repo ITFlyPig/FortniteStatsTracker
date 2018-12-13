683da489bae079f533512790ab41fd799942f798
wangyuelin <a1659509224@163.com>
2018-12-13 22:45:27 +0800

    添加了广告

--- app/src/main/java/com/statstracker/forfornitegame/home/StandingsFragment.java
+++ app/src/main/java/com/statstracker/forfornitegame/home/StandingsFragment.java
@@ -1,26 +1,31 @@
-package com.igameguide.pubg.home;
+package com.statstracker.forfornitegame.home;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v4.app.Fragment;
-import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.TextView;
 
-import com.igameguide.pubg.R;
-import com.igameguide.pubg.detail.DetailActivity;
-import com.igameguide.pubg.util.ConstantValue;
-import com.igameguide.pubg.util.ToastUtil;
+import com.google.android.gms.ads.AdListener;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.InterstitialAd;
+import com.statstracker.forfornitegame.R;
+import com.statstracker.forfornitegame.detail.DetailActivity;
+import com.statstracker.forfornitegame.util.ConstantValue;
 
 
 @SuppressLint("ValidFragment")
 public class StandingsFragment extends Fragment implements View.OnClickListener {
     TextView tvQuery;
     EditText etInput;
+    AdView mAdView;
+    private InterstitialAd mInterstitialAd;
 
 
     public static StandingsFragment getInstance() {
@@ -39,6 +44,17 @@ public class StandingsFragment extends Fragment implements View.OnClickListener 
         tvQuery = v.findViewById(R.id.tv_query);
         tvQuery.setOnClickListener(this);
         etInput = v.findViewById(R.id.et_input_role);
+        mAdView = v.findViewById(R.id.adView);
+        mInterstitialAd = new InterstitialAd(getActivity());
+        mInterstitialAd.setAdUnitId(getResources().getString(R.string.ADUnitID_Screen));
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                loadScreenAd();
+            }
+        }, 5000);
+
 
         return v;
     }
@@ -56,7 +72,31 @@ public class StandingsFragment extends Fragment implements View.OnClickListener 
                 Intent intent = new Intent(getContext(), DetailActivity.class);
                 intent.putExtra(ConstantValue.IntentKey.PLAYER_NAME, playerName);
                 startActivity(intent);
+
+                loadScreenAd();
                 break;
         }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
+    }
+
+    /**
+     * 加载插屏页广告
+     */
+    private void loadScreenAd() {
+        mInterstitialAd.setAdListener(new AdListener() {
+            @Override
+            public void onAdLoaded() {
+                super.onAdLoaded();
+                mInterstitialAd.show();
+            }
+        });
+        mInterstitialAd.loadAd(new AdRequest.Builder().build());
+
     }
 }
